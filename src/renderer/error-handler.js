ab9202345a92caa27a99114c4767e577fc02a454
Chris Mitchell <cbm3384@gmail.com>
2018-04-19 19:19:28 -0600

    use _.isFunction

--- src/renderer/error-handler.js
+++ src/renderer/error-handler.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import _ from 'lodash';
 
 const prev = Vue.config.errorHandler;
 const handler = (err, vm, info) => {
-  if (typeof prev === 'function') {
+  // Do stuff here...
+
+  if (_.isFunction(prev)) {
     prev.call(this, err, vm, info);
   }
 };
