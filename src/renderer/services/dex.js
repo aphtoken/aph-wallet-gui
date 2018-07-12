7b53f082b4f2b3d2801f78ad6738b9dfd1e4831e
James Hollister <james@westleysys.com>
2018-07-12 11:21:55 -0700

    Improvements to order book histogram view

--- src/renderer/services/dex.js
+++ src/renderer/services/dex.js
@@ -91,12 +91,14 @@ export default {
     let runningAsks = new BigNumber(0);
     book.asks.forEach((l) => {
       runningAsks = runningAsks.plus(l.quantity);
-      l.quantityRatio = runningAsks.dividedBy(totalAsk);
+      l.quantityTotalRatio = runningAsks.dividedBy(totalAsk);
+      l.quantityRatio = l.quantity.dividedBy(totalAsk);
     });
     let runningBids = new BigNumber(0);
     book.bids.forEach((l) => {
       runningBids = runningBids.plus(l.quantity);
-      l.quantityRatio = runningBids.dividedBy(totalBid);
+      l.quantityTotalRatio = runningBids.dividedBy(totalBid);
+      l.quantityRatio = l.quantity.dividedBy(totalBid);
     });
 
     return book;
