9af2785d8a17ca6cb54b88f83dbdd1fdcd1fd2aa
Chris Mitchell <cbm3384@gmail.com>
2018-08-26 14:20:08 -0600

    wip

--- test/unit/specs/renderer/components/PortfolioHeader.spec.js
+++ test/unit/specs/renderer/components/PortfolioHeader.spec.js
@@ -5,8 +5,11 @@ import PortfolioHeader from '@/components/PortfolioHeader';
 import utils from './utils';
 
 let actions;
+let getters;
 let store;
+let wrapper;
 
+const address = 'address';
 const i18n = utils.getI18n({
   en: {
     myPortfolio: 'My Portfolio',
@@ -15,27 +18,43 @@ const i18n = utils.getI18n({
 
 describe('PortfolioHeader.vue', () => {
   beforeEach(() => {
-    actions = {
-      fetchPortfolio: _.noop,
-    };
-
     store = new Vuex.Store({
+      actions: {
+        fetchPortfolio: _.noop,
+      },
+      getters: {
+        showSendAddressModal(state) {
+          return state.showSendAddressModal;
+        },
+      },
+      mutations: {
+        setShowSendAddressModal(state, value) {
+          return state.showSendAddressModal = value;
+        },
+      },
       state: {
         portfolio: {
           balance: 200,
           changePercent: 50,
           changeValue: 100,
         },
+        showSendAddressModal: false,
       },
-      actions,
     });
+
+    wrapper = utils.mount(PortfolioHeader, { i18n, store });
+    wrapper.vm.$services.wallets.setCurrentWallet({ address });
   });
 
   it('should render correct contents', () => {
-    const wrapper = utils.shallow(PortfolioHeader, { i18n, store });
-
     expect(wrapper.find('h1.underlined').text()).contains('My Portfolio');
     expect(wrapper.find('.balance .amount').text()).contains('$200.00');
     expect(wrapper.find('.change .amount').text()).contains('$100.00');
+  });
+
+  it('should show show address modal', () => {
+    wrapper.find('.receive-btn').trigger('click');
+
+    expect(wrapper.contains('#aph-address-modal')).to.be.true;
   });
 });
