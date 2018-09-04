aphtoken/aph-wallet-gui

commit ab056fa6eee93dee447f7ea339107d460619715c

Send State Fix -> Network Change maintains 'sendInProgress' flag. (#77)
* 'Send' to address state management fixes

* 'Network Change' maintains 'SendInProgress' state.

* SendInProgress and SendModel state indexed by network

* Code Cleanup (WIP)

src/renderer/components/dashboard/Send.vue
@@ -34,7 +34,7 @@
           </div>
         </div>
       </div>
-      <div class="waiting" v-if="$store.state.sendInProgress === true">{{$t('waitingForTransaction')}}</div>
+      <div class="waiting" v-if="sendInProgress">{{$t('waitingForTransaction')}}</div>
       <div class="footer">
         <button class="back-btn" @click="showConfirmation = false" :disabled="sending">{{$t('back')}}</button>
         <button class="send-btn" @click="send()" :disabled="sending">{{ sendButtonLabel }}</button>
@@ -112,6 +112,7 @@ export default {
       return !this.address || !this.amount || !this.currency || !parseFloat(this.amount);
     },
     ...mapGetters([
+      'sendInProgress',
       'sendModel',
     ]),
   },
@@ -229,7 +230,7 @@ export default {
   },
 
   mounted() {
-    if (this.$store.state.sendInProgress) {
+    if (this.sendInProgress) {
       this.address = this.sendModel.address;
       this.amount = this.sendModel.amount;
       this.contact = this.sendModel.contact;
@@ -239,8 +240,8 @@ export default {
     }
 
     storeUnwatch = this.$store.watch(
-      () => {
-        return this.$store.state.sendInProgress;
+      (state, getters) => {
+        return getters.sendInProgress;
       }, (sendInProgress) => {
         this.$store.commit('setSendModel', !sendInProgress ? {} : {
           address: this.address,
@@ -251,7 +252,7 @@ export default {
           sending: this.sending,
         });
 
-        if (sendInProgress === false && this.sending === true) {
+        if (!sendInProgress && this.sending) {
           this.end();
         }
       });

src/renderer/store/mutations.js
@@ -96,7 +96,7 @@ function handleLogout(state) {
   state.recentTransactions = [];
   state.searchTransactions = [];
   state.nep5Balances = {};
-  state.sendInProgress = false;
+  state.sendInProgress = {};
   state.currentMarket = null;
   neo.fetchNEP5Tokens();
 }
@@ -106,7 +106,6 @@ function handleNetworkChange(state) {
   state.recentTransactions = [];
   state.searchTransactions = [];
   state.nep5Balances = {};
-  state.sendInProgress = false;
   state.currentMarket = null;
   neo.fetchNEP5Tokens();
 }
@@ -308,11 +307,11 @@ function setShowSendRequestLedgerSignature(state, value) {
 }
 
 function setSendInProgress(state, value) {
-  state.sendInProgress = value;
+  Vue.set(state.sendInProgress, state.currentNetwork.net, value);
 }
 
 function setSendModel(state, value) {
-  state.sendModel = value;
+  Vue.set(state.sendModel, state.currentNetwork.net, value);
 }
 
 function setShowWalletBackupModal(state, value) {

src/renderer/components/PortfolioHeader.vue
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import AddressModal from './modals/AddressModal';
 import SimpleDonut from './charts/SimpleDonut';
 import Zoom from './Zoom';
@@ -39,6 +40,12 @@ export default {
     AddressModal,
     SimpleDonut,
     Zoom,
+  },
+
+  computed: {
+    ...mapGetters([
+      'sendInProgress',
+    ]),
   },
 
   methods: {
@@ -59,7 +66,7 @@ export default {
     },
 
     showSendView() {
-      this.$router.push(`/authenticated/dashboard/${this.$store.state.sendInProgress ? 'confirming' : 'send'}`);
+      this.$router.push(`/authenticated/dashboard/${this.sendInProgress ? 'confirming' : 'send'}`);
     },
   },
 

src/renderer/components/dashboard/Holdings.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 let loadHoldingsIntervalId;
 export default {
   computed: {
@@ -18,6 +19,10 @@ export default {
         return !!name && !!symbol;
       });
     },
+
+    ...mapGetters([
+      'sendInProgress',
+    ]),
   },
 
   methods: {
@@ -30,7 +35,7 @@ export default {
     },
 
     viewHoldingDetail(holding) {
-      if (this.$store.state.sendInProgress === true) {
+      if (this.sendInProgress) {
         return;
       }
 

src/renderer/components/dashboard/RecentTransactions.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 let loadTransactionsIntervalId;
 
 export default {
@@ -39,6 +40,10 @@ export default {
           });
         });
     },
+
+    ...mapGetters([
+      'sendInProgress',
+    ]),
   },
 
   methods: {
@@ -51,7 +56,7 @@ export default {
     },
 
     viewTransaction({ details }) {
-      if (this.$store.state.sendInProgress === true) {
+      if (this.sendInProgress) {
         return;
       }
       this.$router.replace('/authenticated/dashboard');

src/renderer/store/getters.js
@@ -25,8 +25,8 @@ export const requests = state => state.requests;
 export const searchTransactionFromDate = state => state.searchTransactionFromDate;
 export const searchTransactionToDate = state => state.searchTransactionToDate;
 export const searchTransactions = state => state.searchTransactions;
-export const sendInProgress = state => state.sendInProgress;
-export const sendModel = state => state.sendModel;
+export const sendInProgress = state => state.sendInProgress[state.currentNetwork.net];
+export const sendModel = state => state.sendModel[state.currentNetwork.net];
 export const showAddContactModal = state => state.showAddContactModal;
 export const showAddTokenModal = state => state.showAddTokenModal;
 export const showOrderConfirmationModal = state => state.showOrderConfirmationModal;

src/renderer/store/index.js
@@ -42,7 +42,7 @@ const state = {
   searchTransactionFromDate: moment().startOf('day').subtract(7, 'days'),
   searchTransactionToDate: null,
   searchTransactions: [],
-  sendInProgress: false,
+  sendInProgress: {},
   sendModel: {},
   showAddContactModal: false,
   showAddTokenModal: false,

commit a53d06b1c1dcd9ba3584be53407eec7cb15e6a32

Fix to send bug
Regression from https://github.com/aphtoken/aph-wallet-gui/commit/ab056fa6eee93dee447f7ea339107d460619715c#diff-1aeb71c0d2d93241a4cbf955d72172e9
Prevents bug that would cause users to send multiple times, but now waits for NEP5 transfers to get confirmed by neoscan rather than not waiting. Still better, will figure out the issue with that shortly.

src/renderer/components/dashboard/Send.vue
@@ -207,7 +207,7 @@ export default {
           })
           .catch((e) => {
             this.sending = false;
-            this.$services.alerts.error(e);
+            this.$services.alerts.exception(e);
             this.$store.commit('setSendInProgress', false);
           });
       }, this.$constants.timeouts.NEO_API_CALL);

src/renderer/store/mutations.js
@@ -357,10 +357,17 @@ function setShowSendRequestLedgerSignature(state, value) {
 }
 
 function setSendInProgress(state, value) {
+  if (!state.sendInProgress || typeof state.sendInProgress !== 'object') {
+    // an invalid or undefined value here is preventing the setting of the variable here
+    state.sendInProgress = {};
+  }
   Vue.set(state.sendInProgress, state.currentNetwork.net, value);
 }
 
 function setSendModel(state, value) {
+  if (!state.sendModel || typeof state.sendModel !== 'object') {
+    state.sendModel = {};
+  }
   Vue.set(state.sendModel, state.currentNetwork.net, value);
 }
 